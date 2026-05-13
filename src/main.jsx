f814f2ca7cc89e318fba4eb81d7f433205df2d18
23f2002684 <23f2002684@ds.study.iitm.ac.in>
2026-05-13 23:37:10 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MobileCompanion from '../MobileCompanion'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MobileCompanion />
+  </React.StrictMode>
+)
