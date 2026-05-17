28ccbb1163689f25ec13cf389fe34f8e3f65c34f
23f2002684 <23f2002684@ds.study.iitm.ac.in>
2026-05-17 11:14:05 +0530

    feat: mobile swipe companion

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MobileCompanion from '../MobileCompanion'
+import MobileCompanion from './MobileCompanion'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
